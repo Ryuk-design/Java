97f98d26c2fda055a0ee25810089ac6159a21dcd
Samuel Ahumada <samuel.ahumada-f@mail.escuelaing.edu.co>
2026-01-02 15:21:48 -0500

    Learning file

---
+++ Learning.java
@@ -0,0 +1,76 @@
+
+/*public class Learning {
+    public static void main(String[] args) {
+        System.out.println("10" + 5); // When adding strings and numbers, the result is a concatenation.
+    }
+}
+*/
+
+/*public class Learning {
+    public static void main(String[] args) {
+         // to declare variables of the same type in one line, write the tipe and then separate the variables usig commas.
+        final String firstname = "Samuel ", lastname = "Ahumada", name = firstname + lastname; 
+        // to declare a variable that cannot be changed, use the final word before the type.
+        System.out.println("first name: " + firstname);
+        System.out.println("last name: " + lastname);
+        System.out.println("name: " + name);
+        int age = 18;
+        System.out.println("age: " + age);
+        char lettertypeob = 'O';
+        System.out.println("letter of type of blood: " + lettertypeob);
+    }
+}
+*/
+
+/*public class Learning {
+    public static void main(String[] args) {
+         @SuppressWarnings("unused")
+       // assign the same value to multiple variables in one line.
+         final int X, Y, Z;
+            X = Y = Z = 50;
+            // by convention, constants are written in uppercase letters.
+    }
+}
+*/
+
+/*public class Learning {
+    public static void main(String[] args) {
+         // calculate the area of a rectangle.
+        int length = 4;
+        int width = 6;
+        int area = (length * width);
+        System.out.println("Length is: " + length);
+        System.out.println("Width is: " + width);
+        System.out.println("Area of the rectangle is: " + area); 
+    }
+}
+*/
+
+/*public class Learning{
+    public static  void main(String[] args) {
+        byte myByte = 127;
+        short myShort = 32000;
+        int myInt = 2000000000;
+        long myLong = 9000000000L;
+        float myFloat = 5.75f; 
+        double myDouble = 19.99d;
+
+    }
+}
+*/
+
+/*public class Learning{
+    public static void main(String[] args) {
+    // Create variables of different data types
+    int items = 50;
+    float costPerItem = 9.99f;
+    float totalCost = items * costPerItem;
+    char currency = 36; // ASCII value for '$'
+    System.out.println("Number of items: " + items);
+    System.out.println("Cost per item: " + costPerItem + currency);
+    System.out.println("Total cost = " + totalCost + currency);
+  }
+}
+*/
+
+    
